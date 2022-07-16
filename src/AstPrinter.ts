46240dbd75d0124987d7badff730b49db1ba0033
Calvin Cheng <calvin.cc.cheng@gmail.com>
2022-07-16 13:32:20 +0800

    feat(parser): add reverse polish notation (RPN) printer (challenge 5.3)

--- src/AstPrinter.ts
+++ src/AstPrinter.ts
@@ -28,17 +28,57 @@ class AstPrinter implements Visitor<string> {
   }
 }
 
+/**
+ * Chapter 5, Challenge 3
+ *
+ * Define a visitor class for our syntax tree classes that takes an expression,
+ * converts it to RPN, and returns the resulting string.
+ *
+ * For example,
+ * (1 + 2) * (4 - 3) becomes 1 2 + 4 3 - *
+ */
+class AstRpnPrinter implements Visitor<string> {
+  print(expr: Expr): string {
+    return expr.accept(this);
+  }
+
+  visitUnaryExpr(expr: Unary): string {
+    return this.rpnize(expr.operator.lexeme, expr.right);
+  }
+
+  visitBinaryExpr(expr: Binary): string {
+    return this.rpnize(expr.operator.lexeme, expr.left, expr.right);
+  }
+
+  visitGroupingExpr(expr: Grouping): string {
+    return this.rpnize("group", expr.expression);
+  }
+
+  visitLiteralExpr(expr: Literal): string {
+    return expr.value === null ? "nil" : String(expr.value);
+  }
+
+  rpnize(name: string, ...exprs: Expr[]): string {
+    return `${exprs.map((expr) => `${expr.accept(this)}`).join(" ")} ${name}`;
+  }
+}
+
 function main(): void {
+  const printer = new AstRpnPrinter();
   const expression = new Binary(
     new Binary(
       new Literal(1),
       new Token(TokenType.PLUS, "+", null, 1),
-      new Literal(4)
+      new Literal(2)
     ),
-    new Token(TokenType.EQUAL_EQUAL, "==", null, 1),
-    new Literal(5)
+    new Token(TokenType.STAR, "*", null, 1),
+    new Binary(
+      new Literal(4),
+      new Token(TokenType.MINUS, "-", null, 1),
+      new Literal(3)
+    )
   );
-  console.log(new AstPrinter().print(expression));
+  console.log(printer.print(expression));
 }
 
 main();
