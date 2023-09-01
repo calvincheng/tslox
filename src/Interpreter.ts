e9d72d7d490c70dcc5ecc1f12b1cbd1e827e9b4e
Calvin Cheng <calvin.cc.cheng@gmail.com>
2023-09-02 02:06:40 +0800

    feat(interpreter): detect runtime errors

--- src/Interpreter.ts
+++ src/Interpreter.ts
@@ -6,6 +6,8 @@
 
 import { Expr, Literal, Grouping, Unary, Binary, Visitor } from "../src/Ast";
 import { TokenType } from "./TokenType";
+import Token from "./Token";
+import { RuntimeError } from "./ErrorHandler";
 
 type LoxObject = Object | null;
 
@@ -24,6 +26,7 @@ export class Interpreter implements Visitor<LoxObject> {
       case TokenType.BANG:
         return !this.isTruthy(right);
       case TokenType.MINUS:
+        this.checkNumberOperand(expr.operator, right);
         return -Number(right);
     }
 
@@ -37,6 +40,7 @@ export class Interpreter implements Visitor<LoxObject> {
 
     switch (expr.operator.type) {
       case TokenType.MINUS:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) - Number(right);
       case TokenType.PLUS:
         if (left instanceof Number && right instanceof Number) {
@@ -45,18 +49,27 @@ export class Interpreter implements Visitor<LoxObject> {
         if (left instanceof String && right instanceof String) {
           return String(left) + String(right);
         }
-        break;
+        throw new RuntimeError(
+          expr.operator,
+          "Operands must be two numbers or two strings."
+        );
       case TokenType.STAR:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) * Number(right);
       case TokenType.SLASH:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) / Number(right);
       case TokenType.GREATER:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) > Number(right);
       case TokenType.GREATER_EQUAL:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) >= Number(right);
       case TokenType.LESS:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) < Number(right);
       case TokenType.LESS_EQUAL:
+        this.checkNumberOperands(expr.operator, left, right);
         return Number(left) <= Number(right);
       case TokenType.BANG_EQUAL:
         return !this.isEqual(left, right);
@@ -67,6 +80,8 @@ export class Interpreter implements Visitor<LoxObject> {
     // Unreachable
     return null;
   }
+
+  // Private methods -- evaluation
 
   private isTruthy(object: LoxObject): boolean {
     if (object == null) return false;
@@ -82,5 +97,21 @@ export class Interpreter implements Visitor<LoxObject> {
 
   private evaluate(expr: Expr): LoxObject {
     return expr.accept(this);
+  }
+
+  // Private methods -- detecting runtime errors
+
+  private checkNumberOperand(operator: Token, operand: LoxObject) {
+    if (operand instanceof Number) return;
+    throw new RuntimeError(operator, "Operand must be a number.");
+  }
+
+  private checkNumberOperands(
+    operator: Token,
+    left: LoxObject,
+    right: LoxObject
+  ) {
+    if (left instanceof Number && right instanceof Number) return;
+    throw new RuntimeError(operator, "Operands must be numbers.");
   }
 }
