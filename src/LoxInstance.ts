dcd02c11d2f14a63760936055456daa8af4573ae
Calvin Cheng <calvin.cc.cheng@gmail.com>
2023-09-25 00:01:36 +0800

    feat: add fields to LoxInstances

--- src/LoxInstance.ts
+++ src/LoxInstance.ts
@@ -1,7 +1,11 @@
 import LoxClass from "./LoxClass";
+import { LoxObject } from "./Interpreter";
+import Token from "./Token";
+import { RuntimeError } from "./ErrorHandler";
 
 export default class LoxInstance {
   private klass: LoxClass;
+  private fields = new Map<string, LoxObject>();
 
   constructor(klass: LoxClass) {
     this.klass = klass;
@@ -9,5 +13,12 @@ export default class LoxInstance {
 
   toString(): string {
     return `${this.klass.name} instance`;
+  }
+
+  get(name: Token): LoxObject {
+    if (this.fields.has(name.lexeme)) {
+      return this.fields.get(name.lexeme)!;
+    }
+    throw new RuntimeError(name, `Undefined property '${name.lexeme}'.`);
   }
 }
